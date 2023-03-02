58d92e935236bc3b03616e1fee2adb1917bb1486
Fadgiras <mayeuxgudinaflorian@gmail.com>
2023-03-02 16:49:55 +0100

    Prettier fixes and Fish class created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,35 @@
   <div class="home">
     <header></header>
     <main>
-      Le projet fonctionne
-      <div>{{ $store.state.fish.length }} fishes</div>
+      <div class="grid grid-cols-3 gap-4 m-8">
+        <div
+          v-for="fish in $store.state.fishArray"
+          :key="fish.id"
+          class="bloc bg-blue-200 p-4 w-max-content shadow-sm border rounded outline outline-1 hover:bg-blue-300"
+        >
+          <div>
+            {{ fish.name }}
+          </div>
+          <div class="text-sm italic font-light">
+            {{
+              fish.comments == ""
+                ? "Pas de description supplémentaire"
+                : fish.comments
+            }}
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'HomeView',
+  name: "HomeView",
   components: {},
-})
+});
 </script>
 
 <style>
@@ -24,10 +40,10 @@ header {
   top: 0;
   position: sticky;
   min-height: 120px;
-  background: #33afaf;
+  background: #3c7a1f;
 }
 main {
   overflow-y: hidden;
-  font-size: 120px;
+  font-size: 20px;
 }
 </style>
