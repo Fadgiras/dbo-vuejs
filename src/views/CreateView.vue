19627dd8aa1ff4ee2af0fd63d320f9c57eb5f412
Alexandre <reisemh3@gmail.com>
2023-03-30 02:05:41 +0200

    ok

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,39 +1,45 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
   <main>
+
     <div>
-      <div class="bloc p-4 max-w-[400px]">
-        <div class="text-3xl pb-16 w-max">Create Fish</div>
-        <div class="wrapper">
-          <div class="min-w-max">Fish name :</div>
-          <input required class="txt" v-model="name" />
-          <div class="min-w-max">Fish category</div>
-          <select class="txt" v-model="category">
-            <option class="txt" v-for="(value, name) in categories" :value="value">
-              {{ name }}
-            </option>
-          </select>
-          <div class="min-w-max">Sale</div>
-          <input required class="check" type="checkbox" name="On sale" v-model="sale" />
-          <div class="min-w-max">Fish Comment</div>
-          <textarea class="txt mb-2 resize" v-model="comments" />
-          <div class="min-w-max">Fish Price</div>
-          <input required class="txt" min="0" type="number" step="any" v-model="price" />
-          <div class="min-w-max">Fish Discount</div>
-          <input required class="txt" min="0" type="number" step="any" v-model="discount" />
-          <div class="min-w-max">Discount price</div>
-          <div>
-            {{ (price - (discount / 100) * price).toFixed(2) }}
-          </div>
-          <div class="min-w-max">Fish unit</div>
-          <input required class="txt" v-model="unit" />
-          <div class="min-w-max">Stock quantity</div>
-          <input required class="txt" min="0" type="number" v-model="quantityInStock" />
+      <div class="header">Create Fish</div>
+      <div class="inputs">
+        <!-- <div class="wrapper"> -->
+        <div class="min-w-max input bg-greyd">Fish name</div>
+        <input required class="txt input" v-model="name" />
+        <div class="min-w-max input bg-greyd">Fish category</div>
+        <select class="txt input" v-model="category">
+          <option class="txt" v-for="(value, name) in categories" :value="value">
+            {{ name }}
+          </option>
+        </select>
+        <div class="min-w-max input bg-greyd">Sale</div>
+        <input required class="check" type="checkbox" name="On sale" v-model="sale" />
+        <div class="min-w-max input bg-greyd">Fish Comment</div>
+        <textarea class="txt mb-2 resize input" v-model="comments"></textarea>
+        <div class="min-w-max input bg-greyd">Fish Price</div>
+        <input required class="txt input" min="0" type="number" step="any" v-model="price" />
+        <div class="min-w-max input bg-greyd">Fish Discount</div>
+        <input required class="txt input" min="0" type="number" step="any" v-model="discount" />
+        <div class="min-w-max input bg-greyd">Discount price</div>
+        <div class="input">
+          {{ (price - (discount / 100) * price).toFixed(2) }}
         </div>
+        <div class="min-w-max input bg-greyd">Fish unit</div>
+        <input required class="txt input" v-model="unit" />
+        <div class="min-w-max input bg-greyd">Stock quantity</div>
+        <input required class="txt input" min="0" type="number" v-model="quantityInStock" />
       </div>
-      <div class="p-4">
-        <button class="button button-red" v-on:click="$router.back()">Back</button>
-        <button class="delete-btn button-green" v-on:click="create()">
+    </div>
+    <div class="flex space-x-4">
+      <div class="p-2">
+        <button class="button input button-red" v-on:click="$router.back()">Back</button>
+      </div>
+    </div>
+    <div class="flex space-x-4">
+      <div class="p-2">
+        <button class="delete-btn input button-green" v-on:click="create()">
           Confirm Create
         </button>
       </div>
@@ -110,4 +116,159 @@ export default defineComponent({
 </script>
 
 <style>
+/* Form style */
+.form {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  border-radius: 0.75rem;
+  background-color: #fff;
+  color: rgb(97 97 97);
+  box-shadow: 20px 20px 30px rgba(0, 0, 0, .05);
+  width: 22rem;
+  background-clip: border-box;
+}
+
+.header {
+  position: relative;
+  background-clip: border-box;
+  background-color: #1e88e5;
+  background-image: linear-gradient(to top right, #1e88e5, #42a5f5);
+  margin: 10px;
+  border-radius: 0.75rem;
+  overflow: hidden;
+  color: #fff;
+  box-shadow: 0 0 #0000, 0 0 #0000, 0 0 #0000, 0 0 #0000, rgba(33, 150, 243, .4);
+  height: 7rem;
+  letter-spacing: 0;
+  line-height: 1.375;
+  font-weight: 600;
+  font-size: 1.9rem;
+  font-family: Roboto, sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.inputs {
+  padding: 1.5rem;
+  gap: 1rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  min-width: 200px;
+  width: 100%;
+  height: 2.75rem;
+  position: relative;
+}
+
+.input {
+  border: 1px solid rgba(128, 128, 128, 0.61);
+  outline: 0;
+  color: rgb(69 90 100);
+  font-weight: 400;
+  font-size: .9rem;
+  line-height: 1.25rem;
+  padding: 0.75rem;
+  background-color: transparent;
+  border-radius: .375rem;
+  width: 100%;
+  height: 100%;
+}
+
+.input:focus {
+  border: 1px solid #1e88e5;
+}
+
+.checkbox-container {
+  margin-left: -0.625rem;
+  display: inline-flex;
+  align-items: center;
+}
+
+.checkbox {
+  position: relative;
+  overflow: hidden;
+  padding: .55rem;
+  border-radius: 999px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.027);
+  height: 35px;
+  width: 35px;
+}
+
+.checkbox input {
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+}
+
+.checkbox-text {
+  cursor: pointer;
+}
+
+.sigin-btn {
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: .75rem;
+  line-height: 1rem;
+  text-align: center;
+  padding: .75rem 1.5rem;
+  background-color: #1e88e5;
+  background-image: linear-gradient(to top right, #1e88e5, #42a5f5);
+  border-radius: .5rem;
+  width: 100%;
+  outline: 0;
+  border: 0;
+  color: #fff;
+}
+
+.signup-link {
+  line-height: 1.5;
+  font-weight: 300;
+  font-size: 0.875rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.signup-link a,
+.forget {
+  line-height: 1.5;
+  font-weight: 700;
+  font-size: .875rem;
+  margin-left: .25rem;
+  color: #1e88e5;
+}
+
+.forget {
+  text-align: right;
+  font-weight: 600;
+}
+
+/* ############# */
+
+/* Checkbox */
+.bg-greyd {
+  letter-spacing: 0;
+  line-height: 1.375;
+  font-weight: 600;
+  font-size: .8rem;
+  font-family: Roboto, sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  background-color: rgb(226 232 240);
+}
+
+/* ############# */
 </style>
